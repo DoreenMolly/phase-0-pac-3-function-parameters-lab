2aafe82472d992217c5a51fc0cba1d302e421410
Doreen <wanjirudoreen13@gmail.com>
2022-06-03 16:46:00 +0300

    commit tree 94646af47765ea7bf4376a2545f012f5713609f6

---
+++ index.js
@@ -0,0 +1,10 @@
+function introduction (Doreen){
+    return (`Hi, my name is ${Doreen}.`)
+}
+
+function introductionWithLanguage (name, language){
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
+function introductionWithLanguageOptional (name, language = "JavaScript"){
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
